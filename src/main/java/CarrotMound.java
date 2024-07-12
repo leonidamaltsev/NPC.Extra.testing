a910f30c78e8ae2cdbf883859d96df1ab3b12789
leonidamaltsev <daf.dof@yandex.ru>
2024-07-12 18:32:32 +0300

    CarrotMound

--- src/main/java/CarrotMound.java
+++ src/main/java/CarrotMound.java
@@ -9,13 +9,8 @@ public class CarrotMound {
         System.arraycopy(clearing, 0, remainingCarrots, 0, clearing.length);
 
         for (int walkers = 1; walkers <= numWalkers; walkers++) {
-            if (walkers > 1) {
-                totalWeight = 0;
-            }
-
             if (totalWeight < maxLoad) {
                 int maxIndex = findMaxIndex(remainingCarrots);
-
                 if (remainingCarrots[maxIndex] > 0) {
                     int takenCarrot = Math.min(maxLoad - totalWeight, remainingCarrots[maxIndex]);
                     totalWeight += takenCarrot;
@@ -34,7 +29,7 @@ public class CarrotMound {
 
     private static int findMaxIndex(int[] array) {
         int maxIndex = 0;
-        for (int i = 1; i < array.length; i++) {
+        for (int i = 0; i < array.length; i++) {
             if (array[i] > array[maxIndex]) {
                 maxIndex = i;
             }
